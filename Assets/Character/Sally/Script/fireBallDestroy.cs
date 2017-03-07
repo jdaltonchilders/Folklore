01a72a26052d3fac53389c693553d9e581efaf02
Dalton Childers <dalton.childers@ssrc.msstate.edu>
2017-03-06 22:46:10 -0600

    Updated Village Map
    Signed-off-by: Dalton Childers <dalton.childers@ssrc.msstate.edu>

---
+++ Assets/Character/Sally/Script/fireBallDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBallDestroy : MonoBehaviour {
+
+	public float spellDelay;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, spellDelay);
+	}
+
+	void OnCollisionEnter2D (GameObject gobject, Collision2D col){
+		Destroy (gobject);
+	}
+}
--- Assets/skeletonAI.cs
+++ Assets/skeletonAI.cs
@@ -9,6 +9,7 @@ public class skeletonAI : MonoBehaviour
 	private float yDiff;
 	private Animator anim;
 	private Rigidbody2D myRigidbody;
+	private Collider2D col2d;
 	private float minDistance = 1.0f;
 	private float maxDistance = 2.0f;
 
@@ -19,16 +20,17 @@ public class skeletonAI : MonoBehaviour
 	{
 		myRigidbody = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
+		col2d = GetComponent<Collider2D> ();
 		health = 100;
 	}
 
 	void Update()
 	{
-		Player = GameObject.Find ("Player").transform.position;
 		if (anim.GetBool ("isDead") == true)
 		{
 			return;
 		}
+		Player = GameObject.FindWithTag("Player").transform.position;
 		xDiff = Player.x - transform.position.x;
 		yDiff = Player.y - transform.position.y;
 
@@ -50,13 +52,21 @@ public class skeletonAI : MonoBehaviour
 
 	void OnCollisionEnter2D (Collision2D col)
 	{
-		if (col.gameObject.tag == "Player")
-		{
-			health -= 20;
-			Debug.Log ("Hit");
-			if (health <= 0)
-			{
+		if (col.gameObject.tag == "Fireball") {
+			Destroy (col.gameObject);
+			health -= 50;
+			if (health <= 0) {
 				anim.SetBool ("isDead", true);
+				Destroy (gameObject);
+			}
+		}
+
+		if (col.gameObject.tag == "LightningBolt") {
+			Destroy (col.gameObject);
+			health -= 100;
+			if (health <= 0) {
+				anim.SetBool ("isDead", true);
+				Destroy (gameObject);
 			}
 		}
 	}
